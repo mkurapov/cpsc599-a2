45b7e4bb61ddcf38248b475b05a7e23122befe64
Max Kurapov <maxkurapov@gmail.com>
2019-02-28 14:31:11 -0700

    working lights

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,31 +8,26 @@ public class PlayerController : MonoBehaviour
 {
     private Animator anim;
     private AndroidJavaObject plugin;
-    private Rigidbody rb;
     private bool isAbleToMove = false;
     private bool isFinished = false;
     private float speed = 0.4f;
-    private Light gameLight;
 
     private GameObject currentTarget;
     private int targetIndex = 0;
 
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody>();
-        //gameLight = GetComponent<Light>();
-
 #if UNITY_ANDROID
         plugin = new AndroidJavaClass("jp.kshoji.unity.sensor.UnitySensorPlugin").CallStatic<AndroidJavaObject>("getInstance");
         plugin.Call("startSensorListening", "accelerometer");
         plugin.Call("startSensorListening", "light");
-        plugin.Call("startSensorListening", "gyroscope");
 #endif
     }
 
     void Update()
     {
         // this is dirty, but Easy AR needs it on each update:
+
         var target1 = GameObject.FindGameObjectWithTag("Target1");
         var target2 = GameObject.FindGameObjectWithTag("Target2");
         var target3 = GameObject.FindGameObjectWithTag("Target3");
@@ -40,7 +35,9 @@ public class PlayerController : MonoBehaviour
         List<GameObject> targets = new List<GameObject> { target1, target2, target3 };
         var isSeeingAllTargets = targets.TrueForAll(t => t && t.activeSelf);
 
-#if UNITY_ANDROID
+        var headlights = GameObject.Find("Headlights").GetComponent<Light>();
+
+        #if UNITY_ANDROID
         if (plugin != null)
         {
             float[] lightValues = plugin.Call<float[]>("getSensorValues", "light");
@@ -48,27 +45,16 @@ public class PlayerController : MonoBehaviour
 
             if (lightValues != null)
             {
-                //gameLight.intensity = 0.1f * lightValues[0];
-                Debug.Log(lightValues[0]);
-                //isAbleToMove = lightValues[0] < 5.0f; //only one value is provided
+                headlights.intensity = lightValues[0] < 5.0f ? 2 : 0;
             }
 
             if (accValues != null)
             {
-                Debug.Log(accValues[1]);
-                isAbleToMove = accValues[1] < 8.8f; //use y value to act as gas pedal
+                isAbleToMove = accValues[1] < 9.3f; //use y value to act as gas pedal
 
-                //Debug.Log("acc:" + string.Join(",", new List<float>(gyroValues).ConvertAll(i => i.ToString()).ToArray()[1]) );
             }
-
-            //if (sensorValues != null)
-            //    {
-
-            //        Debug.Log("sensorValues:" + string.Join(",", new List<float>(sensorValues).ConvertAll(i => i.ToString()).ToArray()));
-            //    }
-            //}
         }
-#endif
+        #endif
 
         if (isSeeingAllTargets && isAbleToMove && !isFinished)
         {
@@ -81,7 +67,7 @@ public class PlayerController : MonoBehaviour
                 targetIndex++;
                 if (targetIndex == targets.Count)
                 {
-                    isFinished = false;
+                    isFinished = true;
                 }
             }
 
